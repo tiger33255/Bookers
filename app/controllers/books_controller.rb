dbb93d94f96fbfe85ddb3425e400adfa68363b91
Taiga Yoshizaki <taiga@Taigas-macbook.local>
2019-07-09 00:07:12 +0900

    add Bookers

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,38 @@
+class BooksController < ApplicationController
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def index
+  	@books = Book.all
+  end
+
+  def new
+  	@book = Book.new
+  	@books = Book.all
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def create
+  	book = Book.new(book_params)
+  	book.save
+  	flash[:success]='book was successfully created.'
+  	redirect_to book_path(book.id)
+  end
+
+  def update
+  	book = Book.find(params[:id])
+  	book.update(book_params)
+  	flash[:success]='book was successfully created.'
+  	redirect_to book_path(book.id)
+  end
+
+  private
+
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+	root to: 'books#index'
+
+	resources :books
+
+end
